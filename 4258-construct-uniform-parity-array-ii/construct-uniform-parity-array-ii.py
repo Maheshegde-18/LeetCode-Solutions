cc64052ec7ba63cf94c8ec0cdbb2bc27306db292
Maheshegde-18 <maheshegde18@gmail.com>
2026-09-03 17:51:32 +0530

    Time: 83 ms (53.85%) | Memory: 21.9 MB (15.38%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def uniformArray(self, nums1):
+        min_odd=float('inf')
+        for num in nums1:
+            if num%2!=0 and num<min_odd:
+                min_odd=num
+        if min_odd==float('inf'):
+            return True
+        for num in nums1:
+            if num%2==0 and num<min_odd:
+                return False
+        return True
+            
+        
+        
